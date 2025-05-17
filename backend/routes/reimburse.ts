3bbac188a9ba91508a2a6f666c5d3cae00e27cd7
catfo435 <pranavlsgreat210120@gmail.com>
2025-05-17 14:20:35 +0530

    fix: missed a lambda to nsdl rename

--- backend/routes/reimburse.ts
+++ backend/routes/reimburse.ts
@@ -34,8 +34,8 @@ router.get('/:projectId', async (req, res) => {
         if (exportData) {
             const workbook = new Workbook()
 
-            workbook.creator = 'LAMBDA Lab, BITS Hyderabad'
-            workbook.lastModifiedBy = 'LAMBDA Lab, BITS Hyderabad'
+            workbook.creator = 'Nano Scale Device Lab, BITS Hyderabad'
+            workbook.lastModifiedBy = 'Nano Scale Device Lab, BITS Hyderabad'
             workbook.created = new Date()
             workbook.modified = new Date()
 
